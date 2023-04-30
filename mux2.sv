4fd5b810efac129c422d7a19b96d89d6ae8d8a3c
BZhang-ucsd <97600878+BZhang-ucsd@users.noreply.github.com>
2023-04-29 22:19:42 -0700

    Update mux2.sv

--- mux2.sv
+++ mux2.sv
@@ -13,10 +13,10 @@ module mux2 #(parameter WIDTH = 8)
 
 always_comb begin
  if (s == 1'b0) begin
-  y[7:0] = d0[7:0]
+  y[7:0] = d0[7:0];
  end
  else begin
-  y[7:0] = d1[7:0]
+  y[7:0] = d1[7:0];
  end
 end
 endmodule
