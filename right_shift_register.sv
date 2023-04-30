413d31fe9fb2a0ccdac822dff625cb364ab4f5a4
BZhang-ucsd <97600878+BZhang-ucsd@users.noreply.github.com>
2023-04-30 00:28:58 -0700

    Update right_shift_register.sv

--- right_shift_register.sv
+++ right_shift_register.sv
@@ -7,27 +7,28 @@ module right_shift_register #(parameter WIDTH = 16)(
     output logic [WIDTH-1:0] out); // shifted input
 
 	
-	always @(posedge clk) begin
+always @(posedge clk) begin
 // fill in the guts	-- holds or shifts by 1 bit position
 // this is a sequential operation, requiring nonblocking (<=) assignments
 // if(...) out <= ...;
 // else if(...) out <= ...;
-//    enable   mode      out  
-//      0       0        hold (no change in output)
-//		0       1	     hold
-//		1       1	     load and logical right shift
-//		1		0	     load and arithmetic right shift
-    end
+//  enable   mode      out  
+//    0       0        hold (no change in output)
+//    0       1	     hold
+//    1       1	     load and logical right shift
+//    1		0	     load and arithmetic right shift
+  if(!enable) begin
+   out <= in
+  end
+  else if (enable && mode) begin
+   out <= {1'b0, in[WIDTH-1:1]};//  load and logical right shift
+  end
+  else if (enable && !mode) begin
+   out <= {in[WIDTH-1], in[WIDTH-1:1]}
+  end
+ end
 
 /* logical right shift fills in 0s from the left
-  logic[3:0] a, b;
-  assign a = 4'b1000;	// = +8 decimal
-  assign b = a>>1;      // b = 4'b0100  = +4 decimal
-// arithmetic right shift preserves MSB (sign bit of a two's comp. number) 
-// note input and output have to be declared type signed (two's comp)
-  logic signed[3:0] a, b;
-  assign a = 4'b1000;   // = -8 decimal
-  assign b = a>>>1;     // b = 4'b1100 = -4 decimal
 // alternative: use concatenation operator {}
   logic[3:0] a, b;
   assign a = 4'b1000;   // = +8 decimal
