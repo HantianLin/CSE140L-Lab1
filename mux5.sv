637846b3d2fe087f77792f42654e985fa3717502
BZhang-ucsd <97600878+BZhang-ucsd@users.noreply.github.com>
2023-04-29 22:15:37 -0700

    Update mux5.sv

--- mux5.sv
+++ mux5.sv
@@ -4,13 +4,13 @@ module mux5 (input        d0, d1, d2, d3, d4,
              output logic y);
 
 always_comb begin   // always @(*)    // always @(d0, d1, d2, d3, d4, s);
-	case(s)
-		3'b000: y = d0;
-      3'b001: y = d1;
-      3'b010: y = d2;
-      3'b011: y = d3;
-      3'b100: y = d4;
-      default: y = 0; // output 0 as default
-    endcase
+ case(s)
+  3'b000: y = d0;
+  3'b001: y = d1;
+  3'b010: y = d2;
+  3'b011: y = d3;
+  3'b100: y = d4;
+  default: y = 0; // output 0 as default
+ endcase
 end
 endmodule
